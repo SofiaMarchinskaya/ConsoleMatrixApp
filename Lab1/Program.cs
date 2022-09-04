04daaa1db95b14d16cb209455fc3931611f46cc7
Sofia Marchinskaya <52950694+SofiaMarchinskaya@users.noreply.github.com>
2022-09-04 22:46:03 +0300

    add safe parser

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,13 +12,28 @@ namespace Lab1
         static void Main(string[] args)
         {
             Console.WriteLine("Rows = ");
-            int n = Int32.Parse(Console.ReadLine());
+            string n_s = Console.ReadLine();
+            int? n = n_s.SafeParseInt();
+            if (n == null) {
+                return;
+            }
             Console.WriteLine("Columns = ");
-            int m = Int32.Parse(Console.ReadLine());
-            double[,] matrix = new double[n,m];
+            n_s = Console.ReadLine();
+            int? m = n_s.SafeParseInt();
+            if (m == null)
+            {
+                return;
+            }
+            double[,] matrix = new double[(int)n, (int)m];
             Program program = new Program();
             Console.WriteLine("Insert matrix. Write rows ");
-            program.ReadMatrix(ref matrix);
+            bool result = program.ReadMatrix(ref matrix);
+            if (!result)
+            {
+                Console.WriteLine("Error occured while data reading. Try Again!");
+                Console.ReadLine();
+                return;
+            }
             Console.WriteLine("Done!");
             Console.WriteLine("Matrix: ");
             program.PrintMatrix(matrix);
@@ -28,7 +44,7 @@ namespace Lab1
             double max = program.Max(matrix);
             Console.WriteLine("Min number in matrix = " + min);
             Console.WriteLine("Max number in matrix = " + max);
-            Console.WriteLine("Max-Min = " + (max-min));
+            Console.WriteLine("Max-Min = " + (max - min));
             if (max - min > p)
             {
                 Console.WriteLine("Max-Min = " + (max - min) + " is greater than p = " + p);
@@ -36,22 +52,23 @@ namespace Lab1
                 Console.WriteLine("Result after replacing: ");
                 program.PrintMatrix(matrix);
             }
-            else {
+            else
+            {
                 Console.WriteLine("Max-Min = " + (max - min) + " is less or equals p = " + p);
-             }
+            }
             Console.ReadLine();
         }
 
         double Min(double[,] matrix)
         {
-            double min = matrix[0,0];
+            double min = matrix[0, 0];
             for (int i = 0; i <= matrix.GetUpperBound(0); i++)
             {
                 for (int j = 0; j <= matrix.GetUpperBound(1); j++)
                 {
-                    if (matrix[i,j] < min)
+                    if (matrix[i, j] < min)
                     {
-                        min = matrix[i,j];
+                        min = matrix[i, j];
                     }
                 }
             }
@@ -60,14 +77,14 @@ namespace Lab1
 
         double Max(double[,] matrix)
         {
-            double max = matrix[0,0];
+            double max = matrix[0, 0];
             for (int i = 0; i <= matrix.GetUpperBound(0); i++)
             {
                 for (int j = 0; j <= matrix.GetUpperBound(1); j++)
                 {
-                    if (matrix[i,j] > max)
+                    if (matrix[i, j] > max)
                     {
-                        max = matrix[i,j];
+                        max = matrix[i, j];
                     }
                 }
             }
@@ -80,41 +97,80 @@ namespace Lab1
             {
                 for (int j = 0; j <= matrix.GetUpperBound(1); j++)
                 {
-                    if (matrix[i,j] > 0)
+                    if (matrix[i, j] > 0)
                     {
-                        matrix[i,j] = 1;
+                        matrix[i, j] = 1;
                     }
                     else
                     {
-                        matrix[i,j] = 0;
+                        matrix[i, j] = 0;
                     }
                 }
             }
 
         }
 
-        void ReadMatrix(ref double[,] matrix)
+        bool ReadMatrix(ref double[,] matrix)
         {
             for (int i = 0; i <= matrix.GetUpperBound(0); i++)
             {
                 string[] s = Console.ReadLine().Split(' ');
                 for (int j = 0; j <= matrix.GetUpperBound(1); j++)
                 {
-                   matrix[i, j] = Double.Parse(s[j]);
+                    double? result = s[j].SafeParseDouble();
+                    if (result == null)
+                    {
+                        return false;
+                    }
+                    matrix[i,j] = (double)result;
                 }
             }
-
+            return true;
         }
-        void PrintMatrix(double[,] matrix) { 
-        for (int i = 0; i <= matrix.GetUpperBound(0); i++)
+
+        void PrintMatrix(double[,] matrix)
+        {
+            for (int i = 0; i <= matrix.GetUpperBound(0); i++)
             {
                 for (int j = 0; j <= matrix.GetUpperBound(1); j++)
                 {
-                    Console.Write(matrix[i,j] + " ");
+                    Console.Write(matrix[i, j] + " ");
                 }
                 Console.WriteLine();
             }
-        
+
+        }
+
+    }
+
+    public static class SafeParser
+    {
+        public static double? SafeParseDouble(this string input)
+        {
+            double output = 0.0;
+            bool result = double.TryParse(input, out output);
+            if (!result)
+            {
+                Console.WriteLine("Parsing error for " + input);
+                Console.ReadLine();
+                return null;
+            }
+            return output;
+        }
+
+
+        public static int? SafeParseInt(this string input)
+        {
+            int output = 0;
+            bool result = int.TryParse(input, out output);
+            if (!result)
+            {
+                Console.WriteLine("Parsing error for " + input);
+                Console.ReadLine();
+                return null;
+            }
+            return output;
         }
     }
 }
+ 
